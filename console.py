4102a59be62bf9ad3bb98e973497d494e17e5540
Dawit Yilma Habtemariam <daveyhmariam@gmail.com>
2024-03-28 12:18:25 +0300

    fix globals

--- console.py
+++ console.py
@@ -1,13 +1,17 @@
 #!/usr/bin/python3
 """command interpreter entry for object interaction"""
-import cmd
 
+from models.base_model import BaseModel
+import cmd
+from models import storage
 
 class HBNBCommand(cmd.Cmd):
     """program called console.py that contains
         the entry point of the command interpreter
     """
     prompt = "(hbnb) "
+    __classes = ["BaseModel",
+                 "FileStorage"]
 
     def do_quit(self, arg):
         """Exit the program
@@ -15,7 +19,10 @@ class HBNBCommand(cmd.Cmd):
         return True
     
     def do_EOF(self, arg):
-        """exit the program
+        """Exit the program
+        
+        Args:
+            arg (str): command line argument
         """
         print("")
         return True
@@ -24,5 +31,22 @@ class HBNBCommand(cmd.Cmd):
         """Do nothing upon receiving an empty line."""
         pass
 
+    def do_create(self, arg):
+        """Creates a new instance of BaseModel,
+            saves it (to the JSON file) and prints the id
+
+        Args:
+            arg (str): command line argument
+        """
+        if len(arg.split()) == 0:
+            print("** class name missing **")
+            return
+        if len(arg.split()) == 1:
+            if arg.split()[0] not in type(self).__classes:
+                print("** class doesn't exist **")
+                return
+            obj = globals()[arg.split()[0]]()
+            print(obj.id)
+            storage.save()
 if __name__ == "__main__":
     HBNBCommand().cmdloop()
